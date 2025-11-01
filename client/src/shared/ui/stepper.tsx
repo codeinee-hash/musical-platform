30c60922656729c2312e5b60bc75c1a619e844ed
Долбеков Элдияр <eldiyar@MacBook-Pro-Dolbekov.local>
2025-11-01 20:46:21 +0600

    refactor

--- client/src/shared/ui/stepper.tsx
+++ client/src/shared/ui/stepper.tsx
@@ -4,21 +4,17 @@ import { ReactNode, useEffect, useState, Children } from "react";
 import { Card, CardBody } from "@heroui/card";
 import { CheckCircle } from "lucide-react";
 
-// Компонент Step
 export function Step({ completed = false, children }: { completed?: boolean; children: ReactNode }) {
     return <div className="flex items-center">{children}</div>;
 }
 
-// Компонент StepLabel
 export function StepLabel({ children }: { children: ReactNode }) {
     return <span className="ml-2 text-sm font-medium">{children}</span>;
 }
 
-// Компонент Stepper
 export function Stepper({ activeStep, children }: { activeStep: number; children: ReactNode }) {
     const [stepsCount, setStepsCount] = useState(0);
 
-    // Подсчёт количества шагов
     useEffect(() => {
         const count = Children.count(children);
         setStepsCount(count);
@@ -34,13 +30,7 @@ export function Stepper({ activeStep, children }: { activeStep: number; children
                         const isLast = index === stepsCount - 1;
 
                         return (
-                            <div
-                                key={index}
-                                className={`${!isLast ? 'flex-1' : ''} flex items-center`}
-                            >
-
-
-                                {/* Индикатор шага */}
+                            <div key={index} className={`${!isLast ? 'flex-1' : ''} flex items-center`}>
                                 <div
                                     className={`w-8 h-8 flex-shrink-0 rounded-full flex items-center justify-center text-sm font-bold transition-colors duration-300 ${
                                         isCompleted
@@ -53,10 +43,8 @@ export function Stepper({ activeStep, children }: { activeStep: number; children
                                     {isCompleted ? <CheckCircle size={16} /> : index + 1}
                                 </div>
 
-                                {/* Метка шага */}
                                 <span className='whitespace-nowrap'>{child}</span>
 
-                                {/* Линия прогресса */}
                                 {!isLast && (
                                     <div className={
                                             `top-5 left-full w-full h-1 transition-all duration-300 ml-4 rounded ${isCompleted ? "bg-primary" : "bg-gray-100"}`
