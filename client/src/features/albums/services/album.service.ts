c8ac805dab040532209699097768d5b2a7cafe50
Долбеков Элдияр <eldiyar@MacBook-Pro-Dolbekov.local>
2025-11-08 02:16:52 +0600

    refactor: services

--- client/src/features/albums/services/album.service.ts
+++ client/src/features/albums/services/album.service.ts
@@ -1,44 +1,41 @@
 import { IAlbum } from "@/shared/lib/types";
 import { requester } from "@/shared/lib/api";
 
-async function getAll(): Promise<IAlbum[]> {
-  try {
-    const { data } = await requester.get<IAlbum[]>("albums");
-    return data;
-  } catch (error) {
-    throw error;
+class AlbumService {
+  async getAll(): Promise<IAlbum[]> {
+    try {
+      const { data } = await requester.get<IAlbum[]>("albums");
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getOne(id: string): Promise<IAlbum> {
+    try {
+      const { data } = await requester.get<IAlbum>(`albums/${id}`);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async create(formData: FormData): Promise<IAlbum> {
+    try {
+      const { data } = await requester.post<IAlbum>("albums", formData);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async remove(id: string): Promise<void> {
+    try {
+      await requester.delete(`albums/${id}`);
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
-async function getOne(id: string): Promise<IAlbum> {
-  try {
-    const { data } = await requester.get<IAlbum>(`albums/${id}`);
-    return data;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function create(formData: FormData): Promise<IAlbum> {
-  try {
-    const { data } = await requester.post<IAlbum>("albums", formData);
-    return data;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function remove(id: string): Promise<void> {
-  try {
-    await requester.delete(`albums/${id}`);
-  } catch (error) {
-    throw error;
-  }
-}
-
-export const albumService = {
-  getAll,
-  getOne,
-  create,
-  remove,
-};
+export const albumService = new AlbumService();
--- client/src/features/musics/services/track.service.ts
+++ client/src/features/musics/services/track.service.ts
@@ -1,57 +1,53 @@
 import { IComment, ITrack } from "@/shared/lib/types";
 import { requester } from "@/shared/lib/api";
 
-async function getTracks(): Promise<ITrack[]> {
-  try {
-    const { data } = await requester.get<ITrack[]>("tracks");
-    return data;
-  } catch (error) {
-    throw error;
+class TrackService {
+  async getTracks(): Promise<ITrack[]> {
+    try {
+      const { data } = await requester.get<ITrack[]>("tracks");
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getOne(id: string): Promise<ITrack> {
+    try {
+      const { data } = await requester.get<ITrack>(`tracks/${id}`);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async createTrack(formData: FormData) {
+    try {
+      await requester.post("tracks", formData);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async addComment(payload: {
+    username: string;
+    text: string;
+    trackId: string;
+  }): Promise<IComment> {
+    try {
+      const { data } = await requester.post("tracks/comment", payload);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteTrack(id: string) {
+    try {
+      await requester.delete(`tracks/${id}`);
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
-async function getOne(id: string): Promise<ITrack> {
-  try {
-    const { data } = await requester.get<ITrack>(`tracks/${id}`);
-    return data;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function createTrack(formData: FormData) {
-  try {
-    await requester.post("tracks", formData);
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function addComment(payload: {
-  username: string;
-  text: string;
-  trackId: string;
-}): Promise<IComment> {
-  try {
-    const { data } = await requester.post("tracks/comment", payload);
-    return data;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function deleteTrack(id: string) {
-  try {
-    await requester.delete(`tracks/${id}`);
-  } catch (error) {
-    throw error;
-  }
-}
-
-export const trackService = {
-  getTracks,
-  getOne,
-  createTrack,
-  deleteTrack,
-  addComment,
-};
+export const trackService = new TrackService();
